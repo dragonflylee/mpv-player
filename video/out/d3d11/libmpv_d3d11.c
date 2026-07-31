a330446e0babb03db17f73f17ba410d56a9d3a89
dragonflylee <dragonflylee@outlook.com>
2026-07-31 15:51:53 +0800

    libmpv:  update d3d11 render api

--- video/out/d3d11/libmpv_d3d11.c
+++ video/out/d3d11/libmpv_d3d11.c
@@ -1,31 +1,44 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "common/msg.h"
-#include "ra_d3d11.h"
-#include "mpv/render_dxgi.h"
-#include "video/out/gpu/libmpv_gpu.h"
+#include "mpv/render_d3d11.h"
 #include "osdep/windows_utils.h"
+#include "ra_d3d11.h"
+#include "video/out/gpu/libmpv_gpu.h"
+#include "video/out/gpu/ra.h"
 
 struct priv {
     ID3D11Device *device;
-    IDXGISwapChain *swapchain;
     struct ra_tex *wrapped_tex;
 };
 
 static int init(struct libmpv_gpu_context *ctx, mpv_render_param *params)
 {
-    MP_VERBOSE(ctx, "Creating libmpv d3d11 context\n");
     struct priv *p = ctx->priv = talloc_zero(NULL, struct priv);
 
-    mpv_dxgi_init_params *init_params =
-        get_mpv_render_param(params, MPV_RENDER_PARAM_DXGI_INIT_PARAMS, NULL);
+    mpv_d3d11_init_params *init_params =
+        get_mpv_render_param(params, MPV_RENDER_PARAM_D3D11_INIT_PARAMS, NULL);
     if (!init_params || !init_params->device)
         return MPV_ERROR_INVALID_PARAMETER;
 
     p->device = init_params->device;
-    p->swapchain = init_params->swapchain;
     ID3D11Device_AddRef(p->device);
-    ID3D11Device_AddRef(p->swapchain);
 
-    // initialize a blank ra_ctx to reuse ra_gl_ctx
     struct ra_ctx *ra_ctx = talloc_zero(p, struct ra_ctx);
     ra_ctx->log = ctx->log;
     ra_ctx->global = ctx->global;
@@ -35,26 +48,33 @@ static int init(struct libmpv_gpu_context *ctx, mpv_render_param *params)
 
     ra_ctx->ra = ra_d3d11_create(p->device, ctx->log, ra_ctx->spirv);
     if (!ra_ctx->ra)
-        return MPV_ERROR_UNSUPPORTED;    
+        return MPV_ERROR_UNSUPPORTED;
 
     ctx->ra_ctx = ra_ctx;
     return 0;
 }
 
-static int wrap_fbo(struct libmpv_gpu_context *ctx, mpv_render_param *params, struct ra_tex **out)
+static int wrap_fbo(struct libmpv_gpu_context *ctx, mpv_render_param *params,
+                    struct ra_tex **out)
 {
     struct priv *p = ctx->priv;
-    ID3D11Resource *backbuffer = NULL;
 
+    mpv_d3d11_fbo *fbo =
+        get_mpv_render_param(params, MPV_RENDER_PARAM_D3D11_FBO, NULL);
+    if (!fbo || !fbo->tex)
+        return MPV_ERROR_INVALID_PARAMETER;
+
+    // Release any previously wrapped texture
+    if (p->wrapped_tex) {
+        ra_tex_free(ctx->ra_ctx->ra, &p->wrapped_tex);
+        p->wrapped_tex = NULL;
+    }
+
+    // Wrap the D3D11 texture as an RA texture
+    p->wrapped_tex = ra_d3d11_wrap_tex(ctx->ra_ctx->ra, 
+            (ID3D11Resource *)fbo->tex);
     if (!p->wrapped_tex) {
-        HRESULT hr = IDXGISwapChain_GetBuffer(p->swapchain, 0, &IID_ID3D11Texture2D,
-                        (void**)&backbuffer);
-        if (FAILED(hr)) {
-            MP_ERR(ctx, "Couldn't get swapchain image\n");
-            return MPV_ERROR_UNSUPPORTED;
-        }
-        p->wrapped_tex = ra_d3d11_wrap_tex(ctx->ra_ctx->ra, backbuffer);
-        SAFE_RELEASE(backbuffer);
+        return MPV_ERROR_UNSUPPORTED;
     }
 
     *out = p->wrapped_tex;
@@ -63,10 +83,7 @@ static int wrap_fbo(struct libmpv_gpu_context *ctx, mpv_render_param *params, st
 
 static void done_frame(struct libmpv_gpu_context *ctx, bool ds)
 {
-    struct priv *p = ctx->priv;
-
     ra_d3d11_flush(ctx->ra_ctx->ra);
-    ra_tex_free(ctx->ra_ctx->ra, &p->wrapped_tex);
 }
 
 static void destroy(struct libmpv_gpu_context *ctx)
@@ -79,13 +96,11 @@ static void destroy(struct libmpv_gpu_context *ctx)
         ctx->ra_ctx->ra->fns->destroy(ctx->ra_ctx->ra);
         ctx->ra_ctx->ra = NULL;
     }
-    
-    SAFE_RELEASE(p->swapchain);
     SAFE_RELEASE(p->device);
 }
 
 const struct libmpv_gpu_context_fns libmpv_gpu_context_d3d11 = {
-    .api_name    = MPV_RENDER_API_TYPE_DXGI,
+    .api_name    = MPV_RENDER_API_TYPE_D3D11,
     .init        = init,
     .wrap_fbo    = wrap_fbo,
     .done_frame  = done_frame,
